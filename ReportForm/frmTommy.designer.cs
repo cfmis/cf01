b8046a9eb81bee71d3d398cf5c4adfaf081becd8
cfmis <leavy_lai@chingfung.com>
2021-10-12 14:55:48 +0800

    test

--- ReportForm/frmTommy.designer.cs
+++ ReportForm/frmTommy.designer.cs
@@ -29,11 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTommy));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.btnImport = new System.Windows.Forms.Button();
             this.dgvDetails = new System.Windows.Forms.DataGridView();
+            this.it_customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trim_code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.season_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.brand_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mo_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.goods_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artwork = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.english_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.size_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.size_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cf_color = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cf_color_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customer_goods = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customer_color_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customer_size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.total_usd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mo_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.total_mo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.lblInformation = new System.Windows.Forms.Label();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,24 +71,6 @@
             this.dataGridViewTextBoxColumn15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.it_customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.trim_code = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.season_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.brand_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.mo_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.goods_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.artwork = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.english_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.size_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.size_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cf_color = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cf_color_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.customer_goods = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.customer_color_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.customer_size = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.total_usd = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.mo_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.total_mo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDetails)).BeginInit();
             this.SuspendLayout();
             // 
@@ -102,9 +102,9 @@
             // 
             this.dgvDetails.AllowUserToAddRows = false;
             this.dgvDetails.AllowUserToDeleteRows = false;
-            this.dgvDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvDetails.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgvDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDetails.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -134,11 +134,148 @@
             this.dgvDetails.TabIndex = 18;
             this.dgvDetails.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dgvDetails_RowPostPaint);
             // 
+            // it_customer
+            // 
+            this.it_customer.DataPropertyName = "it_customer";
+            this.it_customer.HeaderText = "客戶編號";
+            this.it_customer.Name = "it_customer";
+            this.it_customer.ReadOnly = true;
+            this.it_customer.Width = 80;
+            // 
+            // trim_code
+            // 
+            this.trim_code.DataPropertyName = "trim_code";
+            this.trim_code.HeaderText = "Trim Code";
+            this.trim_code.Name = "trim_code";
+            this.trim_code.ReadOnly = true;
+            // 
+            // season_name
+            // 
+            this.season_name.DataPropertyName = "season_name";
+            this.season_name.HeaderText = "季度";
+            this.season_name.Name = "season_name";
+            this.season_name.ReadOnly = true;
+            this.season_name.Width = 40;
+            // 
+            // brand_id
+            // 
+            this.brand_id.DataPropertyName = "brand_id";
+            this.brand_id.HeaderText = "牌子編號";
+            this.brand_id.Name = "brand_id";
+            this.brand_id.ReadOnly = true;
+            this.brand_id.Width = 80;
+            // 
+            // mo_id
+            // 
+            this.mo_id.DataPropertyName = "mo_id";
+            this.mo_id.HeaderText = "頁數";
+            this.mo_id.Name = "mo_id";
+            this.mo_id.ReadOnly = true;
+            this.mo_id.Width = 80;
+            // 
+            // goods_id
+            // 
+            this.goods_id.DataPropertyName = "goods_id";
+            this.goods_id.HeaderText = "產品編號";
+            this.goods_id.Name = "goods_id";
+            this.goods_id.ReadOnly = true;
+            this.goods_id.Width = 120;
+            // 
+            // artwork
+            // 
+            this.artwork.DataPropertyName = "artwork";
+            this.artwork.HeaderText = "圖樣";
+            this.artwork.Name = "artwork";
+            this.artwork.ReadOnly = true;
+            this.artwork.Width = 80;
+            // 
+            // english_name
+            // 
+            this.english_name.DataPropertyName = "english_name";
+            this.english_name.HeaderText = "產品描述";
+            this.english_name.Name = "english_name";
+            this.english_name.ReadOnly = true;
+            this.english_name.Width = 150;
+            // 
+            // size_id
+            // 
+            this.size_id.DataPropertyName = "size_id";
+            this.size_id.HeaderText = "尺寸";
+            this.size_id.Name = "size_id";
+            this.size_id.ReadOnly = true;
+            this.size_id.Width = 50;
+            // 
+            // size_name
+            // 
+            this.size_name.DataPropertyName = "size_name";
+            this.size_name.HeaderText = "尺寸描述";
+            this.size_name.Name = "size_name";
+            this.size_name.ReadOnly = true;
+            // 
+            // cf_color
+            // 
+            this.cf_color.DataPropertyName = "cf_color";
+            this.cf_color.HeaderText = "CF顏色編號";
+            this.cf_color.Name = "cf_color";
+            this.cf_color.ReadOnly = true;
+            // 
+            // cf_color_name
+            // 
+            this.cf_color_name.DataPropertyName = "cf_color_name";
+            this.cf_color_name.HeaderText = "CF顏色描述";
+            this.cf_color_name.Name = "cf_color_name";
+            this.cf_color_name.ReadOnly = true;
+            // 
+            // customer_goods
+            // 
+            this.customer_goods.DataPropertyName = "customer_goods";
+            this.customer_goods.HeaderText = "客產品編號";
+            this.customer_goods.Name = "customer_goods";
+            this.customer_goods.ReadOnly = true;
+            // 
+            // customer_color_id
+            // 
+            this.customer_color_id.DataPropertyName = "customer_color_id";
+            this.customer_color_id.HeaderText = "客顏色編號";
+            this.customer_color_id.Name = "customer_color_id";
+            this.customer_color_id.ReadOnly = true;
+            // 
+            // customer_size
+            // 
+            this.customer_size.DataPropertyName = "customer_size";
+            this.customer_size.HeaderText = "客人尺寸";
+            this.customer_size.Name = "customer_size";
+            this.customer_size.ReadOnly = true;
+            // 
+            // total_usd
+            // 
+            this.total_usd.DataPropertyName = "total_usd";
+            dataGridViewCellStyle1.Format = "N3";
+            dataGridViewCellStyle1.NullValue = null;
+            this.total_usd.DefaultCellStyle = dataGridViewCellStyle1;
+            this.total_usd.HeaderText = "金額(USD)";
+            this.total_usd.Name = "total_usd";
+            this.total_usd.ReadOnly = true;
+            // 
+            // mo_type
+            // 
+            this.mo_type.DataPropertyName = "mo_type";
+            this.mo_type.HeaderText = "頁數類型";
+            this.mo_type.Name = "mo_type";
+            this.mo_type.ReadOnly = true;
+            // 
+            // total_mo
+            // 
+            this.total_mo.DataPropertyName = "total_mo";
+            this.total_mo.HeaderText = "張數";
+            this.total_mo.Name = "total_mo";
+            this.total_mo.ReadOnly = true;
+            // 
             // button1
             // 
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(662, 13);
+            this.button1.Location = new System.Drawing.Point(663, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 37);
             this.button1.TabIndex = 20;
@@ -283,143 +420,6 @@
             this.dataGridViewTextBoxColumn17.HeaderText = "張數";
             this.dataGridViewTextBoxColumn17.Name = "dataGridViewTextBoxColumn17";
             this.dataGridViewTextBoxColumn17.ReadOnly = true;
-            // 
-            // it_customer
-            // 
-            this.it_customer.DataPropertyName = "it_customer";
-            this.it_customer.HeaderText = "客戶編號";
-            this.it_customer.Name = "it_customer";
-            this.it_customer.ReadOnly = true;
-            this.it_customer.Width = 80;
-            // 
-            // trim_code
-            // 
-            this.trim_code.DataPropertyName = "trim_code";
-            this.trim_code.HeaderText = "Trim Code";
-            this.trim_code.Name = "trim_code";
-            this.trim_code.ReadOnly = true;
-            // 
-            // season_name
-            // 
-            this.season_name.DataPropertyName = "season_name";
-            this.season_name.HeaderText = "季度";
-            this.season_name.Name = "season_name";
-            this.season_name.ReadOnly = true;
-            this.season_name.Width = 40;
-            // 
-            // brand_id
-            // 
-            this.brand_id.DataPropertyName = "brand_id";
-            this.brand_id.HeaderText = "牌子編號";
-            this.brand_id.Name = "brand_id";
-            this.brand_id.ReadOnly = true;
-            this.brand_id.Width = 80;
-            // 
-            // mo_id
-            // 
-            this.mo_id.DataPropertyName = "mo_id";
-            this.mo_id.HeaderText = "頁數";
-            this.mo_id.Name = "mo_id";
-            this.mo_id.ReadOnly = true;
-            this.mo_id.Width = 80;
-            // 
-            // goods_id
-            // 
-            this.goods_id.DataPropertyName = "goods_id";
-            this.goods_id.HeaderText = "產品編號";
-            this.goods_id.Name = "goods_id";
-            this.goods_id.ReadOnly = true;
-            this.goods_id.Width = 120;
-            // 
-            // artwork
-            // 
-            this.artwork.DataPropertyName = "artwork";
-            this.artwork.HeaderText = "圖樣";
-            this.artwork.Name = "artwork";
-            this.artwork.ReadOnly = true;
-            this.artwork.Width = 80;
-            // 
-            // english_name
-            // 
-            this.english_name.DataPropertyName = "english_name";
-            this.english_name.HeaderText = "產品描述";
-            this.english_name.Name = "english_name";
-            this.english_name.ReadOnly = true;
-            this.english_name.Width = 150;
-            // 
-            // size_id
-            // 
-            this.size_id.DataPropertyName = "size_id";
-            this.size_id.HeaderText = "尺寸";
-            this.size_id.Name = "size_id";
-            this.size_id.ReadOnly = true;
-            this.size_id.Width = 50;
-            // 
-            // size_name
-            // 
-            this.size_name.DataPropertyName = "size_name";
-            this.size_name.HeaderText = "尺寸描述";
-            this.size_name.Name = "size_name";
-            this.size_name.ReadOnly = true;
-            // 
-            // cf_color
-            // 
-            this.cf_color.DataPropertyName = "cf_color";
-            this.cf_color.HeaderText = "CF顏色編號";
-            this.cf_color.Name = "cf_color";
-            this.cf_color.ReadOnly = true;
-            // 
-            // cf_color_name
-            // 
-            this.cf_color_name.DataPropertyName = "cf_color_name";
-            this.cf_color_name.HeaderText = "CF顏色描述";
-            this.cf_color_name.Name = "cf_color_name";
-            this.cf_color_name.ReadOnly = true;
-            // 
-            // customer_goods
-            // 
-            this.customer_goods.DataPropertyName = "customer_goods";
-            this.customer_goods.HeaderText = "客產品編號";
-            this.customer_goods.Name = "customer_goods";
-            this.customer_goods.ReadOnly = true;
-            // 
-            // customer_color_id
-            // 
-            this.customer_color_id.DataPropertyName = "customer_color_id";
-            this.customer_color_id.HeaderText = "客顏色編號";
-            this.customer_color_id.Name = "customer_color_id";
-            this.customer_color_id.ReadOnly = true;
-            // 
-            // customer_size
-            // 
-            this.customer_size.DataPropertyName = "customer_size";
-            this.customer_size.HeaderText = "客人尺寸";
-            this.customer_size.Name = "customer_size";
-            this.customer_size.ReadOnly = true;
-            // 
-            // total_usd
-            // 
-            this.total_usd.DataPropertyName = "total_usd";
-            dataGridViewCellStyle1.Format = "N3";
-            dataGridViewCellStyle1.NullValue = null;
-            this.total_usd.DefaultCellStyle = dataGridViewCellStyle1;
-            this.total_usd.HeaderText = "金額(USD)";
-            this.total_usd.Name = "total_usd";
-            this.total_usd.ReadOnly = true;
-            // 
-            // mo_type
-            // 
-            this.mo_type.DataPropertyName = "mo_type";
-            this.mo_type.HeaderText = "頁數類型";
-            this.mo_type.Name = "mo_type";
-            this.mo_type.ReadOnly = true;
-            // 
-            // total_mo
-            // 
-            this.total_mo.DataPropertyName = "total_mo";
-            this.total_mo.HeaderText = "張數";
-            this.total_mo.Name = "total_mo";
-            this.total_mo.ReadOnly = true;
             // 
             // frmTommy
             // 
