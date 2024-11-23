ccb105edcb217a7d18b4c03e94daa76e4fabcd2b
allen_leung <allen_leung@DGPC-338>
2024-11-23 11:23:31 +0800

    Update frmPackChanged 2024/11/22

--- ReportForm/frmPackChanged.designer.cs
+++ ReportForm/frmPackChanged.designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle37 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle38 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.chkByCard = new System.Windows.Forms.CheckBox();
-            this.chkmo_print_by_set = new System.Windows.Forms.CheckBox();
+            this.chkByMoPrintSet = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cmbReport = new System.Windows.Forms.ComboBox();
             this.chkByMo = new System.Windows.Forms.CheckBox();
@@ -92,7 +92,7 @@
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.chkByCard);
-            this.panel1.Controls.Add(this.chkmo_print_by_set);
+            this.panel1.Controls.Add(this.chkByMoPrintSet);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.cmbReport);
             this.panel1.Controls.Add(this.chkByMo);
@@ -135,16 +135,16 @@
             this.chkByCard.UseVisualStyleBackColor = true;
             this.chkByCard.Click += new System.EventHandler(this.chByCard_Click);
             // 
-            // chkmo_print_by_set
+            // chkByMoPrintSet
             // 
-            this.chkmo_print_by_set.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.chkmo_print_by_set.Location = new System.Drawing.Point(594, 39);
-            this.chkmo_print_by_set.Name = "chkmo_print_by_set";
-            this.chkmo_print_by_set.Size = new System.Drawing.Size(244, 27);
-            this.chkmo_print_by_set.TabIndex = 52;
-            this.chkmo_print_by_set.Text = "手動輸入頁數套件列印";
-            this.chkmo_print_by_set.UseVisualStyleBackColor = true;
-            this.chkmo_print_by_set.Click += new System.EventHandler(this.chkmo_print_by_set_Click);
+            this.chkByMoPrintSet.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.chkByMoPrintSet.Location = new System.Drawing.Point(594, 39);
+            this.chkByMoPrintSet.Name = "chkByMoPrintSet";
+            this.chkByMoPrintSet.Size = new System.Drawing.Size(244, 27);
+            this.chkByMoPrintSet.TabIndex = 52;
+            this.chkByMoPrintSet.Text = "手動輸入頁數套件列印";
+            this.chkByMoPrintSet.UseVisualStyleBackColor = true;
+            this.chkByMoPrintSet.Click += new System.EventHandler(this.chkByMoPrintSet_Click);
             // 
             // label1
             // 
@@ -484,9 +484,9 @@
             // order_qty_pcs1
             // 
             this.order_qty_pcs1.DataPropertyName = "order_qty_pcs1";
-            dataGridViewCellStyle37.Format = "N0";
-            dataGridViewCellStyle37.NullValue = null;
-            this.order_qty_pcs1.DefaultCellStyle = dataGridViewCellStyle37;
+            dataGridViewCellStyle3.Format = "N0";
+            dataGridViewCellStyle3.NullValue = null;
+            this.order_qty_pcs1.DefaultCellStyle = dataGridViewCellStyle3;
             this.order_qty_pcs1.HeaderText = "訂單數";
             this.order_qty_pcs1.Name = "order_qty_pcs1";
             this.order_qty_pcs1.ReadOnly = true;
@@ -601,9 +601,9 @@
             // dataGridViewTextBoxColumn14
             // 
             this.dataGridViewTextBoxColumn14.DataPropertyName = "order_qty";
-            dataGridViewCellStyle38.Format = "N0";
-            dataGridViewCellStyle38.NullValue = null;
-            this.dataGridViewTextBoxColumn14.DefaultCellStyle = dataGridViewCellStyle38;
+            dataGridViewCellStyle4.Format = "N0";
+            dataGridViewCellStyle4.NullValue = null;
+            this.dataGridViewTextBoxColumn14.DefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridViewTextBoxColumn14.HeaderText = "訂單數";
             this.dataGridViewTextBoxColumn14.Name = "dataGridViewTextBoxColumn14";
             this.dataGridViewTextBoxColumn14.ReadOnly = true;
@@ -678,7 +678,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn goods_name;
         private System.Windows.Forms.DataGridViewTextBoxColumn do_color;
         private System.Windows.Forms.DataGridViewTextBoxColumn order_qty_pcs1;
-        private System.Windows.Forms.CheckBox chkmo_print_by_set;
+        private System.Windows.Forms.CheckBox chkByMoPrintSet;
         private System.Windows.Forms.CheckBox chkByCard;
         private System.Windows.Forms.Label label2;
     }
